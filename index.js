cad893a7cba99a76f449e4ab14403c17dfb20d82
Austin Luft <ausluft@gmail.com>
2018-08-11 02:15:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ function updateObjectWithKeyAndValue (object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object.key = value
   return object
 }
+
+function deleteFromObjectByKey (object, key) {
+  
+}
+
+function 
